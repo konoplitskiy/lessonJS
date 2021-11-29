16d31e1577a6578e95081e3a66707f79d4ed0556
sv.konoplitskii <sv.konoplitskii@gmail.com>
2021-11-29 21:41:43 +0300

    task

---
+++ tasks/app.js
@@ -0,0 +1,18 @@
+// console.log([] + 1 + 2); 12
+//console.log("1"[0]); // 1
+// console.log(2  && 1 && null && 0 && undefined);  null
+
+// И запинается на первой лжи
+// Или запинается на первой правде
+
+// if(!!(a && b) === (a && b)) {
+//     console.log('равно')
+// }
+
+// console.log(null || 2 && 3 || 4);
+
+let a = [1,2,3];
+let b = [1,2,3];
+
+// console.log(typeof Infinity);
+// console.log("Ёжик" > "Яблоко");
